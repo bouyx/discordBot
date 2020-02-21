4a4ed313c137b505e4b7a20c151c35c51a1d3bc7
bouyx <antoine.bouyx@gmail.com>
2020-02-21 22:00:42 +0100

    make function

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const client = new Discord.Client();
 
 var fs = require('fs');
 var words = fs.readFileSync('words.txt').toString().split("\n");
-
+var channelId = '680494155172020271';
 var numberOfPlayer = 0;
 var players = [];
 
@@ -13,45 +13,60 @@ client.on("ready", ()=>{
   console.log('suce');
 })
 client.on("message", function (msg) {
+  console.log(players)
   if (msg.content.match("Start[3-8]")) {
-    numberOfPlayer = parseInt(msg.content.slice(-1));
-    players = [];
+    initGame(msg);
   }
   if (msg.content.match("In")) {
     if (numberOfPlayer === 0) {
       msg.reply("pas de game")
-    } else {
-      players.push(msg.author);
-      numberOfPlayer--;      
+    } else if(players.indexOf(msg.author.username)===-1) {
+      addPlayer(msg);
+      console.log(numberOfPlayer);      
       if (numberOfPlayer === 0) {
-        mot = getRandomInt(words.length);
-        var traitor;
-        var master;
-        do {
-          traitor = getRandomInt(players.length);
-          master = getRandomInt(players.length);
-        } while (master === traitor);
-        client.channels.get('680494155172020271').send("Le MJ sera : ",players[master].username," , ordre ->")
-        for (var i = 0; i < players.length; i++) {
-          if (i === traitor) {
-            players[i].send("Traitor : " + words[mot]);
-            client.channels.get('680494155172020271').send(players[i].username)
-          } else if (i === master) {
-            players[i].send("Maitre : " + words[mot]);
-          } else {
-            players[i].send("Citoyen -> victime");
-            client.channels.get('680494155172020271').send(players[i].username)
-          }
-
-        }
-        setTimeout(response, 1500, words[mot]);
+        startGame();
       }
     }
   }
 });
 
-function response(arg){
-  client.channels.get('680494155172020271').send(arg);
+function startGame(){
+  mot = getRandomInt(words.length);
+  var traitor;
+  var master;
+  do {
+    traitor = getRandomInt(players.length);
+    master = getRandomInt(players.length);
+  } while (master === traitor);
+  client.channels.get(channelId).send("Le MJ sera : "+players[master].username+" , ordre ->")
+  for (var i = 0; i < players.length; i++) {
+    if (i === traitor) {
+      players[i].send("Traitor : " + words[mot]);
+      client.channels.get(channelId).send(players[i].username)
+    } else if (i === master) {
+      players[i].send("Maitre : " + words[mot]);
+    } else {
+      players[i].send("Citoyen -> victime");
+      client.channels.get(channelId).send(players[i].username)
+    }
+  }
+  setTimeout(timeOut, 10000, words[mot]);
+}
+
+function addPlayer(msg){
+  client.channels.get(channelId).send(msg.author.username)
+  players.push(msg.author);
+  numberOfPlayer--;
+}
+
+function initGame(msg){
+  client.channels.get(channelId).bulkDelete(100);
+  client.channels.get(channelId).send("nique")
+  numberOfPlayer = parseInt(msg.content.slice(-1));
+}
+
+function timeOut(arg){
+  client.channels.get(channelId).send("TimeOut : "+arg);
 }
 
 function getRandomInt(max) {
