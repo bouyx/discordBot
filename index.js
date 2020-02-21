17baa97462a3aaa64a115bdc6059470e1f75f74f
bouyx <antoine.bouyx@gmail.com>
2020-02-21 21:00:00 +0100

    custom word

--- index.js
+++ index.js
@@ -1,47 +1,59 @@
 const Discord = require('discord.js');
-const randomWordFR = require('random-word-fr');
 const client = new Discord.Client();
 
-const token = '';
+var fs = require('fs');
+var words = fs.readFileSync('words.txt').toString().split("\n");
+
 var numberOfPlayer = 0;
-var registration = false;
 var players = [];
 
+client.login("NjczNDkyMzc3MTU1MTQxNjYx.XjcgFw.dPT_oi9J7PqtfBt11TIz9Z0jcfA");
 
-bot.on("message", function (msg) {
-	if (msg.content.match("Start[0-6]")) {
-        numberOfPlayer = parseInt(msg.content.slice(-1));
-        registration = true;
+client.on("ready", ()=>{
+  console.log('suce');
+})
+client.on("message", function (msg) {
+  if (msg.content.match("Start[3-8]")) {
+    numberOfPlayer = parseInt(msg.content.slice(-1));
+    players = [];
+  }
+  if (msg.content.match("In")) {
+    if (numberOfPlayer === 0) {
+      msg.reply("pas de game")
+    } else {
+      players.push(msg.author);
+      numberOfPlayer--;      
+      if (numberOfPlayer === 0) {
+        mot = getRandomInt(words.length);
+        var traitor;
+        var master;
+        do {
+          traitor = getRandomInt(players.length);
+          master = getRandomInt(players.length);
+        } while (master === traitor);
+        client.channels.get('680494155172020271').send("Le MJ sera : ",players[master].username," , ordre ->")
+        for (var i = 0; i < players.length; i++) {
+          if (i === traitor) {
+            players[i].send("Traitor : " + words[mot]);
+            client.channels.get('680494155172020271').send(players[i].username)
+          } else if (i === master) {
+            players[i].send("Maitre : " + words[mot]);
+          } else {
+            players[i].send("Citoyen -> victime");
+            client.channels.get('680494155172020271').send(players[i].username)
+          }
+
+        }
+        setTimeout(response, 1500, words[mot]);
+      }
     }
-    if (msg.content.match("In")) {
-        if (numberOfPlayer === 0){
-            msg.reply("pas de game")
-        }else{
-           players.add(msg.author);
-           numberOfPlayer--;
-           if(numberOfPlayer===0){
-                var mot = randomWordFR();
-                var traitor;
-                var master;
-                do {
-                    traitor = getRandomInt(players.length);
-                    master = getRandomInt(players.length);
-                } while (master===traitor);
-                for(var i = 0; i < players.length;i++){
-                    if(i === traitor){
-                        players[i].send("traite :"+mot);
-                    }else if(i === master){
-                        players[i].send("MJ :"+mot);
-                    }else{
-                        players[i].send("citoyen");
-                    }
-                }
-              
-            }
-        }  
-	}
+  }
 });
 
+function response(arg){
+  client.channels.get('680494155172020271').send(arg);
+}
+
 function getRandomInt(max) {
-    return Math.floor(Math.random() * Math.floor(max));
+  return Math.floor(Math.random() * Math.floor(max));
 }
