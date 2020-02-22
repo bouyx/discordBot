7f53de61d2c9335a6e5dcff89dfad2c8adb007b4
Ralin <ralindon@gmail.com>
2020-02-22 02:14:31 +0100

    naze

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ const Discord = require('discord.js');
 const randomWordFR = require('random-word-fr');
 const client = new Discord.Client();
 
+client.login("NjczNDkyMzc3MTU1MTQxNjYx.XjcgFw.dPT_oi9J7PqtfBt11TIz9Z0jcfA");
+
 const token = '';
 var numberOfPlayer = 0;
 var registration = false;
 var players = [];
+var mot = '';
 
-
-bot.on("message", function (msg) {
-	if (msg.content.match("Start[0-6]")) {
+client.on("message", function (msg) {
+	if (msg.content.match("Start[3-8]")) {
+        players=[]
         numberOfPlayer = parseInt(msg.content.slice(-1));
         registration = true;
     }
@@ -17,10 +20,11 @@ bot.on("message", function (msg) {
         if (numberOfPlayer === 0){
             msg.reply("pas de game")
         }else{
-           players.add(msg.author);
+           players.push(msg.author);
            numberOfPlayer--;
+           msg.reply("game joined")
            if(numberOfPlayer===0){
-                var mot = randomWordFR();
+                mot = randomWordFR();
                 var traitor;
                 var master;
                 do {
@@ -29,17 +33,24 @@ bot.on("message", function (msg) {
                 } while (master===traitor);
                 for(var i = 0; i < players.length;i++){
                     if(i === traitor){
-                        players[i].send("traite :"+mot);
+                        players[i].send("traitre : "+mot);
+                        players[i].send("le MJ est : pas fini");
                     }else if(i === master){
                         players[i].send("MJ :"+mot);
                     }else{
                         players[i].send("citoyen");
+                        players[i].send("traitre : pas fini");
                     }
-                }
-              
+                }          
             }
         }  
-	}
+    }
+    if (msg.content.match("End")) {
+        for(var i = 0; i < players.length;i++){
+            players[i].send("reponse : "+mot);
+        }
+
+    }
 });
 
 function getRandomInt(max) {
